bfa8a0df64a89ec64227b9c6ed1240fd28efcc06
mazoal1982 <aloys.manga@gmail.com>
2024-04-02 14:36:37 -0600

    using local connection instead of heroku uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-mongoose.connect(process.env.MONGODB_URI
+mongoose.connect('mongodb+srv://aloysmanga:Amazone1982@cluster0.hf8ubwp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     || 'mongodb://127.0.0.1:27017/googlebooks'
      );
 
