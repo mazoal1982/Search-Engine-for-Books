5cba8f23e566b71b4ef21a90a379525d279f4c5a
mazoal1982 <aloys.manga@gmail.com>
2024-04-02 15:39:04 -0600

    redeploy

--- server/server.js
+++ server/server.js
@@ -23,6 +23,9 @@ app.use(routes);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  });
 }
 
 // Create a new instance of an Apollo server with the GraphQL schema
