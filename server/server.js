16ebe6d335e97f858d7a186a97ba77864ed76579
mazoal1982 <aloys.manga@gmail.com>
2024-04-02 15:59:52 -0600

    git push

--- server/server.js
+++ server/server.js
@@ -22,26 +22,26 @@ app.use(express.json());
 app.use(routes);
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
 }
-app.use(express.static(path.join(__dirname, '../client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-});
 
 // Create a new instance of an Apollo server with the GraphQL schema
-// const startApolloServer = async () => {
-//   await server.start();
-//   server.applyMiddleware({ app });
-//   console.log(startApolloServer)
-// };
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
-
+ const startApolloServer = async () => {
+   await server.start();
+   server.applyMiddleware({ app });
+ //  console.log(startApolloServer)
+ //}
+   db.once('open', () => {
+     app.listen(PORT, () => {
+       console.log(`API server running on port ${PORT}!`);
+       console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      });
+    });
+    
+  };
 // Call the async function to start the server
 //startApolloServer(typeDefs, resolvers);
 
