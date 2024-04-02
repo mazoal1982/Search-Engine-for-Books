2279b57b34a2a825ba4326d8746d3e6ed8dcc785
mazoal1982 <aloys.manga@gmail.com>
2024-04-02 13:18:03 -0600

    added mutations queries and schemas

---
+++ Develop/server/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+// add apollo server
+const routes = require('./routes')
+const { ApolloServer } = require('apollo-server-express'); 
+const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require('./schemas'); 
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+// add apollo middleware
+const server = new ApolloServer({
+  // introspection: true,
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+  //persistedQueries: false,
+}); 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(routes);
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(startApolloServer)
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
+};
+
+// Call the async function to start the server
+//startApolloServer(typeDefs, resolvers);
+
+//const express = require('express');
+//const path = require('path');
+//const db = require('./config/connection');
+//const routes = require('./routes');
+//
+//const app = express();
+//const PORT = process.env.PORT || 3001;
+//
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+//
+//// if we're in production, serve client/build as static assets
+//if (process.env.NODE_ENV === 'production') {
+//  app.use(express.static(path.join(__dirname, '../client/build')));
+//}
+//
+//app.use(routes);
+//
+//db.once('open', () => {
+//  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+//});
